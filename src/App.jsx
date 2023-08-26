a3daf27dd5f61382283241be86c21cd26a7caa6c
FabianPinoP <fpinoperez@outlook.cl>
2023-08-26 13:29:59 -0400

    fix test linter action

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,13 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" rel='noreferrer' target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" rel='noreferrer' target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
