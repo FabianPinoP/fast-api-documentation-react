2387e5ab60220b765948c9a66e58ae70b5f0707b
FabianPinoP <fpinoperez@outlook.cl>
2023-08-25 20:43:19 -0400

    fix bug gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://fabianpinop.github.io/',
+  base: 'https://fabianpinop.github.io/fast-api-documentation-react/',
   plugins: [react()],
 })
